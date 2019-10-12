1b4518a7bacb508d39bbc974342c2553d4321edd
Brownsey <brownsey666@gmail.com>
2019-10-12 23:03:00 +0100

    Had a little look at abundance

--- exploratory.R
+++ exploratory.R
@@ -37,4 +37,35 @@ plotted_map
 
 
 # ~~~ Summarising variables ~~~#
+#Taking a look at how numbers of wild bees compare with 
+abundance_plot <- ggplot(data = data) +
+  geom_point(aes(x = orchard, y = apisAb), colour = "orange") +
+  geom_point(aes(x = orchard, y = wildAbF), colour = "green") +
+  ggtitle("Abundance of honey bees and wild bees vs  temperature") +
+  xlab("Orchard") + 
+  ylab("Abundance Rating")
+abundance_plot
 
+
+abundance_summary <- data %>% summarise(
+  honey_mean = round(mean(apisAb),2),
+  wild_mean = round(mean(wildAbF), 2),
+  honey_sd = round(sd(apisAb), 2),
+  wild_sd = round(sd(wildAbF), 2)
+)
+abundance_summary 
+
+#Looking at temperature vs bees
+temperature_plot <- ggplot(data = data) +
+  geom_point(aes(x = temp, y = apisAb), colour = "orange") +
+  geom_point(aes(x = temp, y = wildAbF), colour = "green") +
+  ggtitle("Abundance of honey bees and wild bees vs temperature") +
+  xlab("Temperature") + 
+  ylab("Abundance Rating")
+temperature_plot
+
+#Looking at the data it seems a quadratic model would be the best fit, shown below
+temperature_plot <- temperature_plot +
+  stat_smooth(aes(x = temp, y = apisAb), method = "lm", formula = y ~ x + I(x^2), colour = "orange") +
+  stat_smooth(aes(x = temp, y = wildAbF), method = "lm", formula = y ~ x + I(x^2), colour = "green") +
+  theme_bw()
