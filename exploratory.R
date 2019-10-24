11c2b66b768012ff8fbe65ae3451ccb96d916d99
Brownsey <brownsey666@gmail.com>
2019-10-24 16:06:38 +0100

    Started looking at decision trees in R, little success

--- exploratory.R
+++ exploratory.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(ggmap)
 library(leaps)
 library(GGally)
+library(DiagrammeR)
 
 #Loading the data in from paper ~ downloaded from their resources as a .xlsx, created csv with useful page for quicker reading.
 data <- read.csv("data.csv") %>%
@@ -195,3 +196,53 @@ wild_by_year_day <- day_data %>%
   xlab("Orchard")
 wild_by_year_day
 
+whole_decision_tree <- grViz("
+digraph dot {
+
+graph [layout = dot,
+       rankdir = LR]
+
+node [shape = circle,
+      style = filled,
+      color = grey,
+      label = '']
+
+node [fillcolor = red]
+a
+
+node [fillcolor = green]
+b c d e
+
+node [fillcolor = orange]
+
+edge [color = grey]
+a -> {b c d e}
+b -> {f g h i j k}
+c -> {l m n o p q}
+d -> {q s t u v w}
+e -> {x y z aa ab ac}
+f -> {ad ae af ag}
+g -> {ah ai aj ak}
+h -> {al am an ao}
+i -> {ap aq ar as}
+j -> {at au av aw}
+k -> {ax ay az ba}
+l -> {bb bc bd be}
+m -> {bf bg bh bi}
+n -> {bj bk bl bm}
+o -> {bn bo bp bq}
+p -> {bs bt bu bv}
+q -> {bw bx by bz}
+r -> {ca cb cc cd}
+s -> {ce cf ch ci}
+t -> {cj ck cl cm}
+u -> {cn co cp cq}
+v -> {cr cs ct cu}
+w -> {cv cw cx cy}
+x -> {cz da db dc}
+y -> {dd de df dg}
+z -> {dh di dj dk}
+aa -> {dl dm dn do}
+ab -> {dp dq dr ds}
+ac -> {ds dt du dv}
+}")
