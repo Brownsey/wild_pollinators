c0afded812eb232c4e7617f29d54910a510a06ba
Brownsey <brownsey666@gmail.com>
2020-03-17 21:46:14 +0000

    may have found a mistake - debugging

---
+++ future_r.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+#Variables we need:
+#orchard, year, visit date, wild abundance, (wild richness), (social abundance), social richness
+data <- tibble(orchard = c(1,1,1), date = c("20/03/2020", "20/05/2020", "20/07/2020"))
